f4648911961e5c85511ccaa4245947155e8de798
MarioHakimm <127972490+MarioHakimm@users.noreply.github.com>
2023-07-26 17:19:05 +0300

    Add files via upload

---
+++ src/Components/Homepage/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Banner.css";
+function Banner() {
+  return (
+    <>
+      <div id="homeBannerMain">
+        {/* <img id="top-cloud" src="./assets/images/cloud.png" alt="cloudImg" /> */}
+        <div>
+          <h1>I'm Mario</h1>
+          <h2>A Full Stack Developer</h2>
+        </div>
+        {/* <img
+          id="bottom-cloud"
+          src="/assets/images/cloud.png"
+          alt="cloud-img"
+        /> */}
+      </div>
+    </>
+  );
+}
+
+export default Banner;
---
+++ src/Components/Homepage/Hello/Hello.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <>
+      <div className='cdiv'>
+        <img id="mypic" src="/assets/Images/MarioPic2.png" alt="My pic" />
+        <h2>Hello</h2>
+        <p id="intro">
+          I am a Full Stack Developer, I have 4+ years of experience, I love to
+          design websites and keep learning new languages to evolve in coding.
+        </p>
+      </div>
+      <hr />
+    </>
+  );
+}
+
+export default Hello
